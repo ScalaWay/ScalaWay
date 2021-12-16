26c6fd10e51e717b68cad35a9e4efcef871df61d
Luidgi Gromat <gromat.luidgi@gmail.com>
2021-12-16 12:07:10 +0100

    Add service collections extensions in shared utils

---
+++ src/ScalaWay.Utils/Extentions/ServiceCollectionExtensions.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScalaWay.Utils.Extentions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection Replace<TService, TImplementation>(
+            this IServiceCollection services,
+            ServiceLifetime lifetime)
+            where TService : class
+            where TImplementation : class, TService
+        {
+            var descriptorToRemove = services.FirstOrDefault(d => d.ServiceType == typeof(TService));
+            if (descriptorToRemove != null)
+            {
+                services.Remove(descriptorToRemove);
+            }
+
+            var descriptorToAdd = new ServiceDescriptor(typeof(TService), typeof(TImplementation), lifetime);
+
+            services.Add(descriptorToAdd);
+
+            return services;
+        }
+
+        public static IServiceCollection Replace<TService>(
+            this IServiceCollection services,
+            Func<IServiceProvider, TService> implementationFactory,
+            ServiceLifetime lifetime)
+            where TService : class
+        {
+            var descriptorToRemove = services.FirstOrDefault(d => d.ServiceType == typeof(TService));
+            if (descriptorToRemove != null)
+            {
+                services.Remove(descriptorToRemove);
+            }
+
+            var descriptorToAdd = new ServiceDescriptor(typeof(TService), implementationFactory, lifetime);
+
+            services.Add(descriptorToAdd);
+
+            return services;
+        }
+
+        public static IServiceCollection Replace(
+            this IServiceCollection services,
+            Type serviceType,
+            Type implementationType,
+            ServiceLifetime lifetime)
+        {
+            var descriptorToRemove = services.FirstOrDefault(d => d.ServiceType == serviceType);
+            if (descriptorToRemove != null)
+            {
+                services.Remove(descriptorToRemove);
+            }
+
+            var descriptorToAdd = new ServiceDescriptor(serviceType, implementationType, lifetime);
+
+            services.Add(descriptorToAdd);
+
+            return services;
+        }
+    }
+}
