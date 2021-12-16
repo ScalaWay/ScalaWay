254186a80b956c70c898b2e8a353110ba3ce4eac
Luidgi Gromat <gromat.luidgi@gmail.com>
2021-12-16 12:07:41 +0100

    Create ILocalizableString.cs

---
+++ src/ScalaWay.Localization/ILocalizableString.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScalaWay.Localization
+{
+    /// <summary>
+    /// Represents a string that can be localized when needed.
+    /// </summary>
+    public interface ILocalizableString
+    {
+        /// <summary>
+        /// Localizes the string in current culture.
+        /// </summary>
+        /// <param name="context">Localization context</param>
+        /// <returns>Localized string</returns>
+        string Localize(ILocalizationContext context);
+
+        /// <summary>
+        /// Localizes the string in given culture.
+        /// </summary>
+        /// <param name="context">Localization context</param>
+        /// <param name="culture">culture</param>
+        /// <returns>Localized string</returns>
+        string Localize(ILocalizationContext context, CultureInfo culture);
+    }
+}
