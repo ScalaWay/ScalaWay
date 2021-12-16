25cc051a34ec8ae265b145c80c15cf6a8751d0a9
Luidgi Gromat <gromat.luidgi@gmail.com>
2021-12-16 12:07:44 +0100

    Create ILocalizationContext.cs

---
+++ src/ScalaWay.Localization/ILocalizationContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScalaWay.Localization
+{
+    /// <summary>
+    /// Localization context.
+    /// </summary>
+    public interface ILocalizationContext
+    {
+    }
+}
